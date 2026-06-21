bddcee66c15bce74533f6da744dfdab86910e1ac
WilliamPLaCroix <williamplacroix@gmail.com>
2026-06-21 09:49:07 +0200

    fix get_engine import path

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy.exc import OperationalError
 from sqlalchemy import text
-from app.db import get_engine
+from app.db.database import get_engine
 
 
 def test_db_connection():
