53cf90788d947ff5fec7d93f218b397bb66f9181
WilliamPLaCroix <williamplacroix@gmail.com>
2026-06-21 16:33:00 +0200

    rewrite card files

--- src/app/modules/cards/create.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from .schemas import CardSchema
-
-# use kwargs to create a card in the database
-def create_card(**kwargs):
-
-    return CardSchema(**kwargs)
---
+++ src/app/modules/cards/repository.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+
+from app.modules.cards.model import CardModel
+
+
+class CardRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, front: str, back: str, deck_id: int) -> CardModel:
+        card = CardModel(
+            front=front,
+            back=back,
+            deck_id=deck_id
+        )
+        self.db.add(card)
+        self.db.commit()
+        self.db.refresh(card)
+        return card
+
+    def get(self, card_id: int) -> CardModel | None:
+        return self.db.query(CardModel).filter(CardModel.id == card_id).first()
+
+    def list_by_deck(self, deck_id: int) -> list[CardModel]:
+        return (
+            self.db.query(CardModel)
+            .filter(CardModel.deck_id == deck_id)
+            .all()
+        )
